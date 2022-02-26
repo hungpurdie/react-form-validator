aca243e09dc3f53aa16970a6274a7e739e27b853
hungpurdie <nmhung2022@gmail.com>
2022-02-27 00:43:18 +0700

    fix: vite build

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,6 +1,9 @@
 import { DOMAttributes } from 'react';
 import React from 'react';
 
+declare module '*.svg' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+}
 declare namespace JSX {
   interface ExtendedButton
     extends React.DetailedHTMLProps<DOMAttributes<HTMLDivElement>, HTMLDivElement> {
